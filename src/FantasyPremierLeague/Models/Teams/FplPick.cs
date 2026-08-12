441da949bf1a48715d4c5c281bdbb0560b83b18e
Akintunde <akin2unde@gmail.com>
2026-08-12 15:56:41 +0100

    Add selling price and purchase price

--- src/FantasyPremierLeague/Models/Teams/FplPick.cs
+++ src/FantasyPremierLeague/Models/Teams/FplPick.cs
@@ -36,4 +36,22 @@ public sealed class FplPick
     /// </summary>
     [JsonPropertyName("is_vice_captain")]
     public bool IsViceCaptain { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of player , def or keeper or forward.
+    /// </summary>
+    [JsonPropertyName("element_type")]
+    public int PlayerType { get; set; }
+
+    /// <summary>
+    /// Gets or sets price.
+    /// </summary>
+    [JsonPropertyName("selling_price")]
+    public float SellingPrice { get; set; }
+
+    /// <summary>
+    /// Gets or sets the purchase price.
+    /// </summary>
+    [JsonPropertyName("purchase_price")]
+    public float Cost { get; set; }
 }
